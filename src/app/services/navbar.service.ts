7d963dd4a93eb259d39746cbbe8de64e3d40ad9f
lmanciagli <leandro.manciagli@gba.gob.ar>
2025-11-12 15:52:54 -0300

    Fix sections by rol en navbar

--- src/app/services/navbar.service.ts
+++ src/app/services/navbar.service.ts
@@ -26,21 +26,21 @@ export class NavbarService {
                 name: 'Mis Proyectos', 
                 route: '/my-projects', 
                 color: 'bg-indigo-600', 
-                availableRoles: ['ONG_PRINCIPAL'] 
+                availableRoles: ['ONG_PRINCIPAL', 'ONG_SUPERVISORA'] 
             },
             { 
                 id: 2, 
                 name: 'Colaboraciones', 
                 route: '/collaborations', 
                 color: 'bg-[#059669]', 
-                availableRoles: ['ONG_COLABORADORA'] 
+                availableRoles: ['ONG_COLABORADORA', 'ONG_SUPERVISORA'] 
             },
             { 
                 id: 3, 
                 name: 'Monitoreo y Seguimiento', 
                 route: '/monitoring', 
                 color: 'bg-orange-500', 
-                availableRoles: ['ONG_GERENCIAL'] 
+                availableRoles: ['ONG_GERENCIAL', 'ONG_SUPERVISORA'] 
             }
         ];
     }
